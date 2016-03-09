880114e5019aa67d0a983ab9e4eef14fc6d1398f
Gavin Meyers <gameyers@cisco.com>
2016-03-09 16:04:53 +0530

    some crappy code for a kafka consumer - needs rethinking and refactoring

--- src/com/ulp/base/EventConsumer.java
+++ src/com/ulp/base/EventConsumer.java
@@ -1,7 +1,50 @@
 package com.ulp.base;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.common.io.Resources;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.*;
+
 /**
  * Created by gameyers on 07/03/16.
  */
 public class EventConsumer {
+    private ObjectMapper mapper = new ObjectMapper();
+    private KafkaConsumer<String, String> consumer;
+    List<String> topics;
+
+    public EventConsumer(){
+        topics = new ArrayList<>();
+    }
+
+    public EventConsumer(String configFilePath, List<String> topics){
+        this();
+        try (InputStream props = Resources.getResource(configFilePath).openStream()) {
+            Properties properties = new Properties();
+            properties.load(props);
+            if (properties.getProperty("group.id") == null) {
+                properties.setProperty("group.id", "group-" + new Random().nextInt(100000));
+            }
+            consumer = new KafkaConsumer<>(properties);
+            this.topics.addAll(topics);
+            consumer.subscribe(this.topics);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    //crappy
+    public EventConsumer subscribeToTopic(String topicName){
+        topics.add(topicName);
+        consumer.subscribe(topics);
+        return this;
+    }
+    //crappy
+    public ConsumerRecords readFromTopic(){
+        return consumer.poll(100);
+    }
+
 }
